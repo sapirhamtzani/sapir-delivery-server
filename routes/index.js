b316debfa65f25aa8d8084e01128cda4e8ba004e
sapirhamtzani <sapirh@wix.com>
2020-07-20 01:46:51 +0300

    new http post func

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,10 @@ router.get('/sapir', (req, res, next) => {
   res.send('Hello from server!!');
 });
 
+router.post('/getMethods',(req, res) => {
+  let address=req.body.address;
+  let zipcode=req.body.zipcode;
+  res.send({success: true, list: `${address} + ${zipcode}`})
+});
+
 module.exports = router;
