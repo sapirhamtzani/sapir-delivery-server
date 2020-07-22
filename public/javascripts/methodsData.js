379e2de7ed78f78df0b8471c9b4b124c06557d9f
sapirhamtzani <sapirh@wix.com>
2020-07-22 12:29:30 +0300

    change api key with heruko

--- public/javascripts/methodsData.js
+++ public/javascripts/methodsData.js
@@ -43,7 +43,7 @@ async function getUserMethods(userObj) {
   const fetchRes = await fetch(
     `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
       address
-    )}&key=AIzaSyDCFJBmM7LO5evgiXA3J0_X3DmGEf9gN_0`
+    )}&key=${process.env.GoogleApi}`
   );
 
   const { results } = await fetchRes.json();
